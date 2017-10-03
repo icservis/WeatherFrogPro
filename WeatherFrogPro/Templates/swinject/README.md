fdf0674d977c7e717d439274dfe8bcd690b90107
Libor Kučera <libor.kucera@zentity.com>
2017-10-03 15:33:12 +0200

    Generamba config

---
+++ WeatherFrogPro/Templates/swinject/README.md
@@ -0,0 +1,2 @@
+# CEB Module
+CEB Module is templates for VIPER architecture in Swift, improoved for CEB Project
--- WeatherFrogPro/scripts/generateUserStoryModule.sh
+++ WeatherFrogPro/scripts/generateUserStoryModule.sh
@@ -4,4 +4,4 @@ echo $testPath
 projectPath="WeatherFrogPro/PresentationLayer/UserStories/$1"
 echo $projectPath
 echo $2
-generamba gen "$2" rviper_controller --test_path "$testPath" --project_file_path "$projectPath" --project_group_path "$projectPath"
+generamba gen "$2" swinject --test_path "$testPath" --project_file_path "$projectPath" --project_group_path "$projectPath"
